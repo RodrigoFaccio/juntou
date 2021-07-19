f517969d16e0344ac9892c5e37b9d196f2ff9fd1
RodrigoFaccio <rodrigo1612fm@gmail.com>
2021-07-19 19:13:37 -0300

    deploy3

--- src/config/database.js
+++ src/config/database.js
@@ -2,10 +2,10 @@ require('dotenv/config');
 
 module.exports = {
     dialect: 'postgres',
-    host: 'localhost',
-    username: 'admin2',
-    password:'juntou1234',
-    database:'juntouApp',
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
     define: {
         timestamps: true,
         underscored: true,
